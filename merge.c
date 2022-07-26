148bcb31e259a12dbb293d617138cba078af9109
SameechaS01 <98073896+SameechaS01@users.noreply.github.com>
2022-07-26 19:08:40 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,42 +1,55 @@
-#include<stdlib.h>
-#include<stdio.h>
-void merge(int arr[], int l, int m, int r)
-{
-int i, j, k;
-int n1 = m - l + 1;
-int n2 = r - m;
-int L[n1], R[n2];
-for (i = 0; i < n1; i++)
-L[i] = arr[l + i];
-for (j = 0; j < n2; j++)
-R[j] = arr[m + 1+ j];
-i = 0;
-j = 0;
-k = l;
-while (i < n1 && j < n2)
-{
-if (L[i] <= R[j])
-{
-arr[k] = L[i];
-i++;
+#include <stdio.h>
+
+#define max 10
+
+int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
+int b[10];
+
+void merging(int low, int mid, int high) {
+   int l1, l2, i;
+
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+      if(a[l1] <= a[l2])
+         b[i] = a[l1++];
+      else
+         b[i] = a[l2++];
+   }
+   
+   while(l1 <= mid)    
+      b[i++] = a[l1++];
+
+   while(l2 <= high)   
+      b[i++] = a[l2++];
+
+   for(i = low; i <= high; i++)
+      a[i] = b[i];
 }
-else
-{
-arr[k] = R[j];
-j++;
+
+void sort(int low, int high) {
+   int mid;
+   
+   if(low < high) {
+      mid = (low + high) / 2;
+      sort(low, mid);
+      sort(mid+1, high);
+      merging(low, mid, high);
+   } else { 
+      return;
+   }   
 }
-k++;
-}
-while (i < n1)
-{
-arr[k] = L[i];
-i++;
-k++;
-}
-while (j < n2)
-{
-arr[k] = R[j];
-j++;
-k++;
-}
+
+int main() { 
+   int i;
+
+   printf("List before sorting\n");
+   
+   for(i = 0; i <= max; i++)
+      printf("%d ", a[i]);
+
+   sort(0, max);
+
+   printf("\nList after sorting\n");
+   
+   for(i = 0; i <= max; i++)
+      printf("%d ", a[i]);
 }
